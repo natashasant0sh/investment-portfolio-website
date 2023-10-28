0073be4f5566d3da3465365f218e4215dbf1dffb
natashasant0sh <natashasantosh13@gmail.com>
2023-10-28 22:30:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Features from './pages/Features';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pages/Features" element={<Features />} />
+        <Route path="/pages/About" element={<About />} />
+        <Route path="/pages/Contact" element={<Contact />} />
+        <Route path="/pages/Login" element={<Login />} />
+        <Route path="/pages/SignUp" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import logo from '../assets/logo.png'
+
+const Navigation = styled.nav`
+  width: 100%;
+  height: 100px; 
+  position: relative;
+  display: flex; 
+  justify-content: space-between; 
+  align-items: center;
+  padding: 0; 
+  `;
+  
+const Logo = styled.img`
+  height: 60px; // adjust as needed
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  justify-content: space-around;
+  gap: 30px; // adjust as needed
+`;
+
+const NavButtons = styled.div`
+  display: flex;
+  gap: 20px; // adjust as needed
+`;
+
+  const NavLink = styled(Link)`
+  color: white; 
+  text-decoration: none; 
+  font-size: 20px;
+  font-family: Roboto Mono;
+  font-weight: 400;
+`;
+
+const NavButton = styled(Link)`
+  color: white; 
+  text-decoration: none; 
+  font-size: 20px;
+  font-family: Roboto Mono;
+  font-weight: 400;
+  border: 3px #32C6DA solid;
+  padding: 10px 20px; 
+  border-radius: 23px;
+`;
+
+const Navbar = () => {
+  return (
+    <Navigation>
+        <Logo src={logo} alt="Logo" />
+        <NavLinks>
+            <NavLink to="./pages/Features">Features</NavLink>
+            <NavLink to="pages/About">About</NavLink>
+            <NavLink to="pages/Contact">Contact</NavLink>
+        </NavLinks>
+        <NavButtons>
+            <NavButton to="pages/Login">Login</NavButton>
+            <NavButton to="pages/SignUp">Sign Up</NavButton>
+        </NavButtons>
+    </Navigation>
+  );
+};
+
+export default Navbar;
+
