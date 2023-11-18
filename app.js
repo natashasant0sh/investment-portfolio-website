1ef85b0634dda2a71e19f79664ddd6110707ff6d
natashasant0sh <natashasantosh13@gmail.com>
2023-11-19 00:06:41 +0530

    password encryption

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
+const bcrypt = require('bcrypt');
+
 
 
 app.get("/pages/Login",cors(),(req,res)=>{
@@ -20,7 +22,9 @@ app.post("/pages/Login", async (req, res) => {
         const user = await collection.findOne({ name: name });
 
         if (user) {
-            if (user.password === password) {
+            // Compare the entered password with the hashed password in the database
+            const validPassword = await bcrypt.compare(password, user.password);
+            if (validPassword) {
                 res.json("exist");
             } else {
                 res.json("wrong password");
@@ -34,13 +38,16 @@ app.post("/pages/Login", async (req, res) => {
 });
 
 
+app.post("/pages/SignUp", async (req, res) => {
+    const { name, password } = req.body;
 
-app.post("/pages/SignUp",async(req,res)=>{
-    const{name,password}=req.body
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-    const data={
-        name:name,
-        password:password
+    const data = {
+        name: name,
+        password: hashedPassword
     }
 
     try{
