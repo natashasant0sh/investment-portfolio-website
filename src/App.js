011e0396b14194263bf2f0572e2497da05540853
nehanair04 <113300406+nehanair04@users.noreply.github.com>
2023-11-21 20:29:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Features from './pages/Features';
@@ -7,28 +7,20 @@ import Contact from './pages/Contact';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
-import StockDetails from "./pages/StockDetails"
-import StockContext from './context/StockContext';
 
 function App() {
-
-  const[stockSymbol, setStockSymbol] = useState("FB");
-
   return (
     <Router>
-      <StockContext.Provider value={{stockSymbol, setStockSymbol}}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/pages/Features" element={<Features />} />
+        <Route path="/pages/Features" element={<Features/>} />
         <Route path="/pages/About" element={<About />} />
         <Route path="/pages/Contact" element={<Contact />} />
         <Route path="/pages/Login" element={<Login />} />
         <Route path="/pages/SignUp" element={<SignUp />} />
-        <Route path="/StockDetails" element ={<StockDetails/>}/>
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-      </StockContext.Provider>
-      </Router>
+    </Router>
   );
 }
 
